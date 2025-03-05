f86d79657924efa5440675e8fddb776848aa5ab8
mayuravartak <kvmayura@gmail.com>
2025-03-05 15:47:10 -0500

    Finalised code

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 import pandas as pd
 import ssl
 
+# to find table number in the wikipedia page
+# wiki = pd.read_html('https://en.wikipedia.org/wiki/Jorge_Martin')
+# for i in range(0,15):
+#     print("################################## Table: ",i)
+#     print(wiki[i])
+
 #GATHERING DATA AND APPENDING 
 tabNo = [6, 8, 6, 7, 6, 6, 7, 7, 8, 10, 7, 6, 7, 9, 7, 7, 6, 9, 7, 5, 7, 8]
 riderNo = [93, 1, 5, 10, 12, 20, 21, 23, 33, 35, 36, 42, 49, 54, 63, 72, 73, 79, 88, 43, 25, 37]
@@ -9,13 +15,6 @@ riderName = ["Marc Marquez", "Jorge Martin", "Johann Zarco", "Luca Marini", "Mav
 riderCountry = ["Spain", "Spain", "France", "Italy", "Spain", "France", "Italy", "Italy", "South Africa", "Thailand", "Spain", "Spain", "Italy", "Spain", "Italy", "Italy", "Spain", "Japan", "Portugal", "Australia", "Spain", "Spain"]
 
 tbRiders = pd.DataFrame()
-
-# to find table number in the wikipedia page
-# wiki = pd.read_html('https://en.wikipedia.org/wiki/Jorge_Martin')
-# for i in range(0,15):
-#     print("##################################",i)
-#     print(wiki[i])
-
 
 for i in range(0,len(tabNo)):
     try:
@@ -44,9 +43,9 @@ tbRiders = tbRiders.rename(columns = {'Number': 'bike_number', 'Rider Name': 'ri
 # tbRiders['points'] = tbRiders['points'].replace({'0*': '0'})
 tbRiders['points'] = tbRiders['points'].astype(str).str.extract('(\\d+)').astype(float).astype('Int64')
 tbRiders['placed'] = tbRiders['placed'].astype(str).str.extract('(\\d+)').astype(float).astype('Int64')
-# .astype(str): Ensures all values are strings before processing.
-# .str.extract('(\d+)'): Extracts only numeric digits from values like "6th", "23rd", etc.
-# .astype(float).astype('Int64'): Converts the extracted number into an integer while handling NaNs.
+# .astype(str) makes all values to string before processing
+# .str.extract('(\d+)') extracts only numeric digits
+# .astype(float).astype('Int64') converts the extracted number into an integer while handling NaNs
 tbRiders['placed'] = tbRiders['placed'].fillna(0)
 tbRiders['world_championships'] = tbRiders['world_championships'].where(tbRiders['world_championships'] == '1', '0')
 
@@ -55,8 +54,14 @@ tbRiders['season'] = tbRiders['season'].astype(int)
 tbRiders['points'] = tbRiders['points'].astype(int)
 tbRiders['world_championships'] = tbRiders['world_championships'].astype(int)
 
-print(tbRiders.dtypes)
+#Reordering the column names
+cols = list(tbRiders.columns)
+cols.insert(2, cols.pop(4))  
+cols.insert(3, cols.pop(4))
+tbRiders = tbRiders[cols]
+
+#Sorting the columns
+tbRiders = tbRiders.sort_values(by=['bike_number', 'season'])
 
 
-
-tbRiders.to_excel("C:/Mayura/Projects/MotoGP-DataAnalysis/RiderDetails.xlsx", index=False)
+tbRiders.to_csv("C:/Mayura/Projects/MotoGP-DataAnalysis/RiderDetails.csv", index=False)
